fa6af129b4c06d512c66c15d0221b77980c04dc8
wuuashen <wuuashen@gmail.com>
2023-04-04 10:55:02 +0800

    for ZF

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,10 +17,10 @@ try {
 }
 
 export const metadata = {
-  title: "ChatGPT Next Web",
+  title: "ChatGPT for ZF",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "ChatGPT for ZF",
     statusBarStyle: "black-translucent",
   },
   themeColor: "#fafafa",
